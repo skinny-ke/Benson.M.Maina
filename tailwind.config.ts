0400ffa5bf55d38ff290ea4542e511a35edbd290
skinny-ke <maishb800@gmail.com>
2025-12-08 17:47:22 +0300

    colours added to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        "gradient-primary": "linear-gradient(to right, #4f46e5, #06b6d4)", // ADDED
+      },
       keyframes: {
         "accordion-down": {
           from: {
